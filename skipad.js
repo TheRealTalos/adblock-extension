1923630bcd96c5d2a5b384605a3db26e8b2f776e
Zach M <wackizacki@gmail.com>
2019-05-29 17:38:33 -0400

    Fixed for new youtube update

--- skipad.js
+++ skipad.js
@@ -5,29 +5,29 @@
     if (window.location.pathname !== '/watch') {
       return;
     }
-    var isVideoAd = document.getElementsByClassName('videoAdUi')[0];
+    var isVideoAd = document.getElementsByClassName('ad-showing')[0];
     var btnCloseBanner = document.getElementsByClassName('svg-close-button')[0];
     var btnSpeedUp = document.getElementById('SpeedUp');
     var btnSpeedReset = document.getElementById('PlayBackRate');
     var btnSpeedDown = document.getElementById('SpeedDown');
-    var btnSkipAd = document.getElementsByClassName('videoAdUiSkipButton')[0];
+    var btnSkipAd = document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0];
     console.log(ad);
     if (btnSkipAd) {
-      //console.log("skipad");
+      console.log("skipad");
       eventFire(btnSkipAd, 'click');
     }
     if (btnCloseBanner){
-      //console.log("closeBanner");
+      console.log("closeBanner");
       eventFire(btnCloseBanner, 'click');
     }
     if (isVideoAd){
       ad = true;
-      //console.log("speedup");
+      console.log("speedup");
       for (var i = 0; i < 64; i++){
         eventFire(btnSpeedUp, 'click');
       }
     }else if (ad){
-      //console.log("button reset");
+      console.log("button reset");
       ad = false;
       eventFire(btnSpeedReset, 'click');
     }
